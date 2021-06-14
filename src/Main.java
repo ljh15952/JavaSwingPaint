27334b7e6425b95b549e6dd54dafb8fe9221c75b
Can I? <ljh15952@gmail.com>
2021-06-15 00:08:37 +0900

    Project Start

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("qweqwe");
+	}
+}
+
+//자바 그림판 만들기 시~작
+
+/*
+TODO
+0. 메뉴바, 툴바의 활용
+0.5 JScrollPane 이용
+1. 도형의 선색
+2. 도형의 선두께
+3. 채움색
+4, 그룹화 이동
+5. 그룹화 복사****
+6. 그룹화 풀기 *****
+7. 저장 및 다시 열기****
+8. API 개발 후 추가 기능
+*/
